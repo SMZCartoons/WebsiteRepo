3bc46c6a77a0fd3d58ab2a88093fd89e694a3695
SMZCartoons <45153041+SMZCartoons@users.noreply.github.com>
2024-04-08 13:21:11 -0400

    Insert actors

--- Labs/Lab10/createActors.sql
+++ Labs/Lab10/createActors.sql
@@ -5,3 +5,10 @@ CREATE TABLE `actors` (
    `dob` date DEFAULT NULL,
    PRIMARY KEY (`actorid`)
 );
+
+INSERT INTO actors
+VALUES (1, "Scarlett", "Johansson", "1959-12-03"),
+   (2, "Chris", "Hemsworth", "1930-11-14"),
+   (3, "Ron", "Livingston", "1900-10-04"),
+   (4, "Shawesh", "Bert", "2000-02-13"),
+   (5, "Hammah", "Roomba", "1980-05-18");
